537b57db656a0124e52cce6528b830d14097c2bf
Vovetta <vkovyzin@yandex.ru>
2024-10-14 22:06:33 +0300

    chages

--- app/repositories/wallet/repository.py
+++ app/repositories/wallet/repository.py
@@ -1,9 +1,12 @@
 from web3 import AsyncWeb3
 
+from app.core.database import Pool
+
 
 class WalletRepository:
-    def __init__(self, blockchain: AsyncWeb3):
+    def __init__(self, blockchain: AsyncWeb3, pool: Pool):
         self.blockchain = blockchain
+        self.pool = pool
 
     async def register_wallet(self) -> None:
         self.blockchain.eth
